6a3364fa1d9b0ef5b10a27d89d40d2dbe808a656
Mikkel <eaamiby@students.eaaa.dk>
2022-10-14 23:46:22 +0200

    added bson

--- BookingHandler/Models/BookingDTO.cs
+++ BookingHandler/Models/BookingDTO.cs
@@ -1,7 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace BookingHandler.Models
 {
     public class BookingDTO
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public long? BookingId { get; set; }
         public string? CustomerName { get; set; }
         public DateTime? StartDate { get; set; }
