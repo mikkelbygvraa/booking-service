a4887f1fe010430fcaf28ea566b17a6ad0900a88
Mikkel <eaamiby@students.eaaa.dk>
2022-10-14 09:08:36 +0200

    Name changes

--- BookingHandler/Program.cs
+++ BookingHandler/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Taxa Booking Handler Service", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Booking Handler Service", Version = "v1" });
 });
 
 builder.Services.AddSingleton<IBookingRepository, BookingRepository>();
--- BookingProducer/Program.cs
+++ BookingProducer/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Taxa Booking Producer Service", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Booking Producer Service", Version = "v1" });
 });
 
 builder.Services.AddSingleton<IMessageService, MessageService>();
