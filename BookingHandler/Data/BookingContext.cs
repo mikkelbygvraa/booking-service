37e25b3a776319334a859690af9238877733b3c1
Mikkel <eaamiby@students.eaaa.dk>
2022-10-14 23:46:08 +0200

    Added mongodb context

---
+++ BookingHandler/Data/BookingContext.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using BookingHandler.Models;
+
+namespace BookingHandler.Data
+{
+    public interface IBookingContext
+    {
+        IMongoCollection<BookingDTO> Bookings { get; }
+    }
+
+    public class BookingContext : IBookingContext
+    {
+        public BookingContext(IConfiguration configuration) 
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var db = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+        }
+
+        public IMongoCollection<BookingDTO> Bookings { get; }
+    }
+}
