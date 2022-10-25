1fb35e4ba8667e219418195ffaca4f2cedadf518
Mikkel <eaamiby@students.eaaa.dk>
2022-10-25 10:00:13 +0200

    Restructuring

--- Booking Handler/BookingHandler.API/Program.cs
+++ Booking Handler/BookingHandler.API/Program.cs
@@ -1,8 +1,8 @@
 using Microsoft.OpenApi.Models;
 
 using BookingHandler.Data;
-using BookingHandler.Repositories;
 using BookingHandler.Services;
+using BookingHandler.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
